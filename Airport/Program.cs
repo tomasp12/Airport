c5eb2edd85d6804ab4119be38e6d33e22a5629ff
tomasp12 <111774777+tomasp12@users.noreply.github.com>
2022-12-10 13:54:33 +0200

    Revert "Done"

--- Airport/Program.cs
+++ Airport/Program.cs
@@ -1,25 +1,2 @@
-﻿using Airport.DB;
-using Airport.Repositories;
-using Airport.Models;
-using Airport;
-
-//Creating DB
-//need Microsoft.Data.Sqlite is a lightweight ADO.NET provider for SQLite.
-var database = new CreateDB();
-
-// Generate Report list from DB
-var reportGenerator = new ReportGenerator();
-var listfromEU = reportGenerator.GenerateReportAircraftInEurope();
-var listfromNotEU = reportGenerator.GenerateReportAircraftNotInEurope();
-
-// Generate Html string from Report list 
-HTMLGenerator htmlGenerator = new HTMLGenerator();
-var htmlReportAirplainFromEu = htmlGenerator.FormatHTML(listfromEU, true);
-var htmlReportAirplainNotFromEu = htmlGenerator.FormatHTML(listfromNotEU, false);
-
-//Sending report emails
-//EmailSender emailSender = new EmailSender();
-//emailSender.EmailSend(htmlReportAirplainFromEu);
-//emailSender.EmailSend(htmlReportAirplainNotFromEu);
-
-Console.WriteLine();
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
