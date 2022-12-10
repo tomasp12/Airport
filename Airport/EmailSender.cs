f1683e70ff67921f6fdb91071eaf848464d197d2
Tomas Pranka <tomas.pranka@codeacademylt.onmicrosoft.com>
2022-12-10 18:29:33 +0200

    Redo email sending using gmail.com, password saving in configuration file

--- Airport/EmailSender.cs
+++ Airport/EmailSender.cs
@@ -5,20 +5,29 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net.Mail;
 using System.Net;
+using System.Configuration;
 
 namespace Airport
 {
     public class EmailSender
-    {
+    {        
         public void EmailSend(string emailBody) 
         {
-            var client = new SmtpClient("smtp.mailtrap.io", 2525)
+            using (MailMessage mail = new MailMessage())
             {
-                Credentials = new NetworkCredential("f9cf1f54d7ffea", "d22f8851584625"),
-                EnableSsl = true
-            };
-            client.Send("airplane@gmail.com", "tomyte@gmail.com", "Incoming airplane report", emailBody);
-                
+                var appSettings = ConfigurationManager.AppSettings;
+                mail.From = new MailAddress(appSettings["emailFromAddress"]);
+                mail.To.Add(appSettings["emailToAddress"]);
+                mail.Subject = "Airplane report";
+                mail.Body = emailBody;
+                mail.IsBodyHtml = true;
+                using (SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587))
+                {                    
+                    smtp.Credentials = new NetworkCredential(appSettings["gmailUsername"], appSettings["gmailPassword"]);
+                    smtp.EnableSsl = true;
+                    smtp.Send(mail);
+                }
+            }
         }
     }
 }
